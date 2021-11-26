af6d74cc731539ca7d1fe15076df84417a01f9ab
Jerry Zheng <u6579712@anu.edu.au>
2021-11-26 15:21:37 +0800

    first commit

---
+++ Sort algo/quick sort.cpp
@@ -0,0 +1,10 @@
+// Created by Tsuna on 2021/11/26.
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    //let's say we have a chaotic sequence
+    int *arr = [];
+}
+
---
+++ cmake-build-debug/CMakeFiles/ALGORITHM.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "ALGORITHM.exe"
+  "ALGORITHM.exe.manifest"
+  "ALGORITHM.pdb"
+  "CMakeFiles/ALGORITHM.dir/Sort_algo/quick_sort.cpp.obj"
+  "libALGORITHM.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ALGORITHM.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
